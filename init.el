85d16bc6278a4adf74ba34d937ebb901c3e6adb2
emre-acar <emre.acar@stelab.com.tr>
2021-04-14 09:26:05 +0300

    null

--- init.el
+++ init.el
@@ -744,14 +744,13 @@
 
 
 ;; js and typescript
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
-;; enable typescript-tslint checker
-(flycheck-add-mode 'typescript-tslint 'web-mode)
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
+
 
 
 (use-package apheleia
