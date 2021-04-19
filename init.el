14bfdd64d73d7ee950ec67b513fd8cf3b94b63d0
emre-acar <emre.acar@stelab.com.tr>
2021-04-19 12:08:54 +0300

    typescript-mode changed

--- init.el
+++ init.el
@@ -81,10 +81,6 @@
 ;;   :ensure t
 ;;   :config (helm-mode 1))
 
-(setq ido-everywhere t)
-(setq ido-enable-flex-maching t)
-(ido-mode t)
-
 (use-package projectile
   :ensure t
   :config
@@ -99,9 +95,9 @@
   :ensure t
   :init
   (progn
-    (setq dashboard-items '((recents . 1)
-			    (projects . 1)
-			    (bookmarks . 2)))
+    (setq dashboard-items '((recents . 3)
+			    (projects . 3)
+			    (bookmarks . 3)))
     ;; (setq dashboard-show-shortcuts nil)
     (setq dashboard-center-content nil)
     (setq dashboard-banner-logo-title "neias")
@@ -201,7 +197,9 @@
   (tide-setup)
   (flycheck-mode +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (tide-hl-identifier-mdoe +1)
+  (tide-hl-identifier-mode +1)
+  ;; editing
+  (setq typescript-indent-level 2)
   (company-mode +1))
 
 (use-package tide
@@ -214,10 +212,97 @@
   :after (typescript-mode)
   :hook (typescript-mdoe . prettier-js-mode))
 
+;; Better Completions with Ivy
+(use-package ivy :ensure t
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-f" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :init
+  (ivy-mode 1)
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-wrap t)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq enable-recursive-minibuffers t)
 
+  ;; Use different regex strategies per completion command
+  (push '(completion-at-point . ivy--regex-fuzzy) ivy-re-builders-alist) ;; This doesn't seem to work...
+  (push '(swiper . ivy--regex-ignore-order) ivy-re-builders-alist)
+  (push '(counsel-M-x . ivy--regex-ignore-order) ivy-re-builders-alist)
 
+  ;; Set minibuffer height for different commands
+  (setf (alist-get 'counsel-projectile-ag ivy-height-alist) 15)
+  (setf (alist-get 'counsel-projectile-rg ivy-height-alist) 15)
+  (setf (alist-get 'swiper ivy-height-alist) 15)
+  (setf (alist-get 'counsel-switch-buffer ivy-height-alist) 7))
 
+(use-package ivy-hydra
+  :defer t
+  :after hydra)
 
+(use-package ivy-rich :ensure t
+  :init
+  (ivy-rich-mode 1)
+  :after counsel
+  :config
+  (setq ivy-format-function #'ivy-format-function-line)
+  (setq ivy-rich-display-transformers-list
+        (plist-put ivy-rich-display-transformers-list
+                   'ivy-switch-buffer
+                   '(:columns
+                     ((ivy-rich-candidate (:width 40))
+                      (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right)); return the buffer indicators
+                      (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))          ; return the major mode info
+                      (ivy-rich-switch-buffer-project (:width 15 :face success))             ; return project name using `projectile'
+                      (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))  ; return file path relative to project root or `default-directory' if project is nil
+                     :predicate
+                     (lambda (cand)
+                       (if-let ((buffer (get-buffer cand)))
+                           ;; Don't mess with EXWM buffers
+                           (with-current-buffer buffer
+                             (not (derived-mode-p 'exwm-mode)))))))))
+
+(use-package counsel :ensure t
+  :demand t
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         ("C-M-j" . counsel-switch-buffer)
+         ("C-M-l" . counsel-imenu)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
+
+(use-package flx  ;; Improves sorting for fuzzy-matched results
+  :after ivy
+  :defer t
+  :init
+  (setq ivy-flx-limit 10000))
+
+(use-package prescient :ensure t
+  :after counsel
+  :config
+  (prescient-persist-mode 1))
+
+(use-package ivy-prescient :ensure t
+  :after prescient
+  :config
+  (ivy-prescient-mode 1))
 
 
 ;; ;; The default is 800 kilobytes.  Measured in bytes.
@@ -257,12 +342,6 @@
 ;; ;; (when dw/is-termux
 ;; ;;   (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
 
-
-;; ;; Initialize use-package on non-Linux platforms
-;; (unless (or (package-installed-p 'use-package)
-;;             dw/is-guix-system)
-;;   (package-install 'use-package))
-;; (require 'use-package)
 
 ;; ;; Uncomment this to get a reading on packages that get loaded at startup
 ;; (setq use-package-verbose t)
@@ -307,10 +386,6 @@
 ;;           (expand-file-name "custom.el" server-socket-dir)
 ;;         (expand-file-name (format "emacs-custom-%s.el" (user-uid)) temporary-file-directory)))
 ;; (load custom-file t)
-
-
-;; ;; Add my library path to load-path
-;; (push "~/.emacs.d/lisp" load-path)
 
 
 ;; (use-package use-package-chords
@@ -468,87 +543,14 @@
 ;; (use-package hydra
 ;;   :defer 1)
 
-;; ;; Better Completions with Ivy
-;; (use-package ivy
-;;   :diminish
-;;   :bind (("C-s" . swiper)
-;;          :map ivy-minibuffer-map
-;;          ("TAB" . ivy-alt-done)
-;;          ("C-f" . ivy-alt-done)
-;;          ("C-l" . ivy-alt-done)
-;;          ("C-j" . ivy-next-line)
-;;          ("C-k" . ivy-previous-line)
-;;          :map ivy-switch-buffer-map
-;;          ("C-k" . ivy-previous-line)
-;;          ("C-l" . ivy-done)
-;;          ("C-d" . ivy-switch-buffer-kill)
-;;          :map ivy-reverse-i-search-map
-;;          ("C-k" . ivy-previous-line)
-;;          ("C-d" . ivy-reverse-i-search-kill))
-;;   :init
-;;   (ivy-mode 1)
-;;   :config
-;;   (setq ivy-use-virtual-buffers t)
-;;   (setq ivy-wrap t)
-;;   (setq ivy-count-format "(%d/%d) ")
-;;   (setq enable-recursive-minibuffers t)
 
-;;   ;; Use different regex strategies per completion command
-;;   (push '(completion-at-point . ivy--regex-fuzzy) ivy-re-builders-alist) ;; This doesn't seem to work...
-;;   (push '(swiper . ivy--regex-ignore-order) ivy-re-builders-alist)
-;;   (push '(counsel-M-x . ivy--regex-ignore-order) ivy-re-builders-alist)
 
-;;   ;; Set minibuffer height for different commands
-;;   (setf (alist-get 'counsel-projectile-ag ivy-height-alist) 15)
-;;   (setf (alist-get 'counsel-projectile-rg ivy-height-alist) 15)
-;;   (setf (alist-get 'swiper ivy-height-alist) 15)
-;;   (setf (alist-get 'counsel-switch-buffer ivy-height-alist) 7))
 
-;; (use-package ivy-hydra
-;;   :defer t
-;;   :after hydra)
 
-;; (use-package ivy-rich
-;;   :init
-;;   (ivy-rich-mode 1)
-;;   :after counsel
-;;   :config
-;;   (setq ivy-format-function #'ivy-format-function-line)
-;;   (setq ivy-rich-display-transformers-list
-;;         (plist-put ivy-rich-display-transformers-list
-;;                    'ivy-switch-buffer
-;;                    '(:columns
-;;                      ((ivy-rich-candidate (:width 40))
-;;                       (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right)); return the buffer indicators
-;;                       (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))          ; return the major mode info
-;;                       (ivy-rich-switch-buffer-project (:width 15 :face success))             ; return project name using `projectile'
-;;                       (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))  ; return file path relative to project root or `default-directory' if project is nil
-;;                      :predicate
-;;                      (lambda (cand)
-;;                        (if-let ((buffer (get-buffer cand)))
-;;                            ;; Don't mess with EXWM buffers
-;;                            (with-current-buffer buffer
-;;                              (not (derived-mode-p 'exwm-mode)))))))))
 
-;; (use-package counsel
-;;   :demand t
-;;   :bind (("M-x" . counsel-M-x)
-;;          ("C-x b" . counsel-ibuffer)
-;;          ("C-x C-f" . counsel-find-file)
-;;          ("C-M-j" . counsel-switch-buffer)
-;;          ("C-M-l" . counsel-imenu)
-;;          :map minibuffer-local-map
-;;          ("C-r" . 'counsel-minibuffer-history))
-;;   :custom
-;;   (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
-;;   :config
-;;   (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
 
-;; (use-package flx  ;; Improves sorting for fuzzy-matched results
-;;   :after ivy
-;;   :defer t
-;;   :init
-;;   (setq ivy-flx-limit 10000))
+
+
 
 ;; (use-package wgrep)
 
@@ -566,15 +568,7 @@
 ;;                                   (right-fringe . 8)))
 ;;   (ivy-posframe-mode 1))
 
-;; (use-package prescient
-;;   :after counsel
-;;   :config
-;;   (prescient-persist-mode 1))
 
-;; (use-package ivy-prescient
-;;   :after prescient
-;;   :config
-;;   (ivy-prescient-mode 1))
 
 
 ;; ;; Completion Annotations with Marginalia
@@ -1055,3 +1049,16 @@
 ;;   :after docker)
 
 ;; ;; (server-start)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ivy-prescient prescient ivy-rich counsel ivy which-key use-package treemacs-projectile tide prettier-js org-bullets helm-projectile expand-region doom-themes doom-modeline dashboard company centaur-tabs)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
