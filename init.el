785458b17563f8bf34575c3a48bf87e973765892
Emre Acar <emre.acar@stelab.com.tr>
2022-02-08 16:09:56 +0300

    new emacs conf

---
+++ init.el
@@ -0,0 +1,272 @@
+;; init.el --- neias Emacs Config
+
+;; Author: Emre Acar <emreacar@hotmail.com>
+;; Version: 0.0.1
+;; Keywords: Emacs, configuration
+;; URL: https://github.com/neias/.emacs.d
+
+;;; Code:
+
+(set-language-environment "English")
+(set-locale-environment "en.UTF-8")
+(prefer-coding-system 'utf-8)
+
+;; Define and initialise package repositories
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; use-package to simplify the config file
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure 't)
+
+(setq js-indent-level 2)
+
+;; Emacs Theme
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-gruvbox t))
+
+(use-package doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode)
+  :custom-face
+  (mode-line ((t (:height 0.85))))
+  (mode-line-inactive ((t (:height 0.85))))
+  :custom
+  (doom-modeline-height 15)
+  (doom-modeline-bar-width 6)
+  (doom-modeline-lsp t)
+  (doom-modeline-github nil)
+  (doom-modeline-mu4e nil)
+  (doom-modeline-irc nil)
+  (doom-modeline-minor-modes t)
+  (doom-modeline-persp-name nil)
+  (doom-modeline-buffer-file-name-style 'truncate-except-project)
+(doom-modeline-major-mode-icon nil))
+
+(use-package all-the-icons ;; M-x all-the-icons-install-fonts RET
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-idle-delay 0.9)
+  :config
+  (which-key-mode))
+
+;; Keyboard-centric user interface
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)          ; Disable the toolbar
+(menu-bar-mode -1)	    ; Disable the menu bar
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(global-hl-line-mode +1)    ; Highlight line
+(delete-selection-mode 1)   ; Delete selection
+;; (tooltip-mode -1)           ; Disable tooltips
+(set-default-coding-systems 'utf-8)
+(setq backup-directory-alist '(("." . "~/.saves")))   ; Different backup directory
+(column-number-mode)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode) ; Display line numbers when programming
+
+;; Emacs dashboard configure
+(use-package dashboard
+  :ensure t
+  :init
+  (progn
+    (setq dashboard-items '((recents . 3)
+			    (projects . 3)
+			    (bookmarks . 3)))
+    (setq dashboard-show-shortcuts nil)
+    (setq dashboard-center-content nil)
+    (setq dashboard-banner-logo-title "neias")
+    (setq dashboard-set-file-icons t)
+    (setq dashboard-set-heading-icons t)
+    (setq dashboard-startup-banner "~/.emacs.d/images/emacs-logo.png")
+    (setq dashboard-set-navigator t)
+    (setq dashboard-navigator-buttons
+	  `(;; linel
+	    ((, nil
+		"init file"
+		"Open init file"
+		(lambda (&rest _) (find-file "~/.emacs.d/init.el"))
+		)))))
+  :config
+  (dashboard-setup-startup-hook))
+
+(use-package swiper
+  :ensure t)
+
+;; Better Completions with Ivy
+(use-package ivy :ensure t
+  :diminish
+  :bind
+  (("C-s" . swiper)
+   :map ivy-minibuffer-map
+   ("TAB" . ivy-alt-done)
+   ("C-f" . ivy-alt-done)
+   ("C-l" . ivy-alt-done)
+   ("C-j" . ivy-next-line)
+   ("C-k" . ivy-previous-line)
+   :map ivy-switch-buffer-map
+   ("C-k" . ivy-previous-line)
+   ("C-l" . ivy-done)
+   ("C-d" . ivy-switch-buffer-kill)
+   :map ivy-reverse-i-search-map
+   ("C-k" . ivy-previous-line)
+   ("C-d" . ivy-reverse-i-search-kill))
+  :init
+  (ivy-mode 1)
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-wrap t)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq enable-recursive-minibuffers t)
+  ;; Use different regex strategies per completion command
+  (push '(completion-at-point . ivy--regex-fuzzy) ivy-re-builders-alist) ;; This doesn't seem to work...
+  (push '(swiper . ivy--regex-ignore-order) ivy-re-builders-alist)
+  (push '(counsel-M-x . ivy--regex-ignore-order) ivy-re-builders-alist)
+
+  ;; Set minibuffer height for different commands
+  (setf (alist-get 'counsel-projectile-ag ivy-height-alist) 15)
+  (setf (alist-get 'counsel-projectile-rg ivy-height-alist) 15)
+  (setf (alist-get 'swiper ivy-height-alist) 15)
+  (setf (alist-get 'counsel-switch-buffer ivy-height-alist) 7))
+
+(use-package counsel
+  :ensure t
+  :demand t
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 ("C-M-j" . counsel-switch-buffer)
+	 ("C-M-l" . counsel-imenu)
+	 :map minibuffer-local-map
+	 ("C-r" . counsel-minibuffer-history))
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
+
+(use-package ivy-rich
+  :ensure t
+  :init
+  (ivy-rich-mode 1)
+  :after counsel
+  :config
+  (setq ivy-format-function #'ivy-format-function-line)
+  (setq ivy-rich-display-transformers-list
+        (plist-put ivy-rich-display-transformers-list
+                   'ivy-switch-buffer
+                   '(:columns
+                     ((ivy-rich-candidate (:width 40))
+                      (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right)); return the buffer indicators
+                      (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))          ; return the major mode info
+                      (ivy-rich-switch-buffer-project (:width 15 :face success))             ; return project name using `projectile'
+                      (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))  ; return file path relative to project root or `default-directory' if project is nil
+                     :predicate
+                     (lambda (cand)
+                       (if-let ((buffer (get-buffer cand)))
+                           ;; Don't mess with EXWM buffers
+                           (with-current-buffer buffer
+                             (not (derived-mode-p 'exwm-mode)))))))))
+
+(use-package flx  ;; Improves sorting for fuzzy-matched results
+  :after ivy
+  :defer t
+  :init
+  (setq ivy-flx-limit 10000))
+
+(use-package prescient :ensure t
+  :after counsel
+  :config
+  (prescient-persist-mode 1))
+
+(use-package ivy-prescient :ensure t
+  :after prescient
+  :config
+  (ivy-prescient-mode 1))
+
+;; Snippets
+ (use-package yasnippet
+       :ensure t
+       :init
+       (yas-global-mode 1)
+       :config
+       (add-to-list 'yas-snippet-dirs (locate-user-emacs-file "snippets")))
+
+;; Window Selection with ace-window
+(use-package ace-window
+  :bind (("M-o" . ace-window))
+  :custom
+  (aw-scope 'frame)
+  (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (aw-minibuffer-flag t)
+  :config
+  (ace-window-display-mode 1))
+
+;; Highlight Matching Braces
+(use-package paren
+  :config
+  (set-face-attribute 'show-paren-match-expression nil :background "#363e4a")
+  (show-paren-mode 1))
+
+(use-package expand-region
+  :ensure t
+  :bind
+  ("C-*" . er/expand-region)
+  ("C--" . er/contract-region))
+
+;; Window tabs
+(use-package centaur-tabs
+  :ensure t
+  :config
+  (setq centaur-tabs-set-bar 'over
+	centaur-tabs-set-icons t
+	centaur-tabs-gray-out-icons 'buffer
+	centaur-tabs-height 24
+	centaur-tabs-set-modified-marker t
+	centaur-tabs-modified-marker "o")
+  (centaur-tabs-mode t))
+
+;; Autocomplate
+(use-package company
+  :ensure t
+  :init
+  (add-hook 'after-init-book 'company-tabnine))
+
+;; Trigger completion immediately.
+(setq company-idle-delay 0)
+
+;; Number the candidates (use M-1, M-2 etc to select completions).
+(setq company-show-numbers t)
+
+(use-package flycheck
+  :ensure t
+  :hook (after-init . global-flycheck-mode))
+
+(use-package php-mode
+  :hook (php-mode . flycheck))
+
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook (typescript-mode . setup-tide-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(tide php-mode flx ace-window ivy-prescient prescient ivy-rich dashboard doom-modeline flycheck company centaur-tabs expand-region all-the-icons doom-themes use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:height 0.85))))
+ '(mode-line-inactive ((t (:height 0.85)))))
