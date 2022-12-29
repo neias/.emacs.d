35e3ea1c0fc46b7cd177965c776d0e2fcd7976ae
Emre Acar <emreacar@Emre-iMac.local>
2022-12-29 17:05:38 +0300

    v0.0.3

--- init.el
+++ init.el
@@ -1,21 +1,49 @@
-;; init.el --- neias Emacs Config
+;;; package --- Summary:
 
-;; Author: Emre Acar <emreacar@hotmail.com>
-;; Version: 0.0.1
-;; Keywords: Emacs, configuration
-;; URL: https://github.com/neias/.emacs.d
+;;; Commentary:
+;;; init.el --- neias Emacs Config
+
+;;; Author: Emre Acar <emreacar@hotmail.com>
+;;; Version: 0.0.3
+;;; Keywords: Emacs, configuration
+;;; URL: https://github.com/neias/.emacs.d
 
 ;;; Code:
-
 (set-language-environment "English")
 (set-locale-environment "en.UTF-8")
 (prefer-coding-system 'utf-8)
 
-(setq ns-alternate-modifier 'meta)
-(setq ns-right-alternate-modifier 'none)
+(setq inhibit-startup-message t) ;; Disable startup message
+(tool-bar-mode -1)               ;; Disable the toolbar
+(menu-bar-mode -1)               ;; Disable the menu bar
+(scroll-bar-mode -1)             ;; Disable visible scrollbar
+(global-hl-line-mode +1)         ;; Highlight line
+(delete-selection-mode 1)        ;; Then inserting text while the mark is active causes the selected text to be deleted first.
+(tooltip-mode -1)                ;; Disable tooltips
+(set-default-coding-systems 'utf-8)
+(setq backup-directory-alist '(("." . "~/.saves")))   ; Different backup director
+(add-hook 'prog-mode-hook 'display-line-numbers-mode) ; Display line numbers when programming
 
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+;; for mac os
+(if (eq system-type 'darwin)
+    (setq mac-command-modifier      'meta
+          mac-option-modifier       'alt
+          mac-right-option-modifier 'super
+					ns-alternate-modifier 'meta
+					ns-right-alternate-modifier 'none))
 
+;; starting resize emacs
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+      (progn
+				(if (> (x-display-pixel-width) 1280)
+            (add-to-list 'default-frame-alist (cons 'width 130))
+          (add-to-list 'default-frame-alist (cons 'width 80)))
+				(add-to-list 'default-frame-alist 
+										 (cons 'height (/ (- (x-display-pixel-height) 200)
+																			(frame-char-height)))))))
+(set-frame-size-according-to-resolution)
 
 ;; Define and initialise package repositories
 (require 'package)
@@ -32,54 +60,148 @@
 (require 'use-package)
 (setq use-package-always-ensure 't)
 
-(setq js-indent-level 2)
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-idle-delay 0.5)
+  :config
+  (which-key-mode))
 
-;; Emacs Theme
-(use-package doom-themes
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)
+	 )
+  )
+
+;; Better Completions with Ivy
+(use-package ivy
+  :ensure t
+  :diminish
+  :init
+  (ivy-mode 1)
+  )
+
+(use-package counsel
+  :ensure t
+  :demand t
+  :bind (("M-x" . counsel-M-x)
+	 ;; ("C-x b" . counsel-ibuffer)
+	 ;; ("C-x C-f" . counsel-find-file)
+	 ("C-M-j" . counsel-switch-buffer)
+	 ("C-M-l" . counsel-imenu)
+	 )
+  )
+
+;;; Sorting and filtering
+(use-package prescient :ensure t
+  :after counsel
+  :config
+  (prescient-persist-mode 1))
+
+(use-package ivy-prescient :ensure t
+  :after prescient
+  :config
+  (ivy-prescient-mode 1))
+
+(use-package ivy-rich
+  :ensure t
+  :init
+  (ivy-rich-mode 1)
+  :after counsel
+  )
+
+(use-package flx  ;; Improves sorting for fuzzy-matched results
+  :after ivy
+  :defer t
+  :init
+  (setq ivy-flx-limit 10000))
+
+
+;; Highlight Matching Braces
+(use-package paren
+  :config
+  (set-face-attribute 'show-paren-match-expression nil :background "#363e4a")
+  (show-paren-mode 1))
+
+(use-package expand-region
+  :ensure t
+  :bind
+  ("C-*" . er/expand-region)
+  ("C--" . er/contract-region))
+
+;; Window tabs
+(use-package centaur-tabs
   :ensure t
   :config
-  (load-theme 'doom-gruvbox t))
+  (setq centaur-tabs-set-bar 'over
+	centaur-tabs-set-icons t
+	centaur-tabs-gray-out-icons 'buffer
+	centaur-tabs-height 24
+	centaur-tabs-set-modified-marker t
+	centaur-tabs-modified-marker "o")
+  (centaur-tabs-mode t))
+
+
+
+;; Window Selection with ace-window
+(use-package ace-window
+  :bind (("M-o" . ace-window))
+  :custom
+  (aw-scope 'frame)
+  (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (aw-minibuffer-flag t)
+  :config
+  (ace-window-display-mode 1))
+
+(use-package gruvbox-theme
+  :ensure t)
+
+;; dark/light theme selecting
+(defun dark-mode ()
+  "Activate dark mode color theme"
+  (interactive)
+  (load-theme 'gruvbox-dark-medium t))
+(global-set-key (kbd "C-c d") 'dark-mode)
+(defun light-mode ()
+  "Activate light mode color theme."
+  (interactive)
+  (load-theme 'gruvbox-light-hard t))
+(global-set-key (kbd "C-c l") 'light-mode)
+
+(use-package all-the-icons ;; M-x all-the-icons-install-fonts RET
+  :ensure t)
 
 (use-package doom-modeline
   :ensure t
   :hook (after-init . doom-modeline-mode)
   :custom-face
   (mode-line ((t (:height 0.85))))
-  (mode-line-inactive ((t (:height 0.85))))
+  ;; (mode-line-inactive ((t (:height 0.85))))
   :custom
   (doom-modeline-height 15)
   (doom-modeline-bar-width 6)
   (doom-modeline-lsp t)
-  (doom-modeline-github nil)
+  (doom-modeline-github t)
   (doom-modeline-mu4e nil)
   (doom-modeline-irc nil)
   (doom-modeline-minor-modes t)
   (doom-modeline-persp-name nil)
   (doom-modeline-buffer-file-name-style 'truncate-except-project)
-(doom-modeline-major-mode-icon nil))
+  (doom-modeline-major-mode-icon nil))
 
-(use-package all-the-icons ;; M-x all-the-icons-install-fonts RET
-  :ensure t)
+;; Selected font 
+(defun set-font (font-name)
+  "Set font for current frame."
+  (interactive "sFont name: ")
+  (set-frame-font font-name))
 
-(use-package which-key
-  :ensure t
-  :init
-  (setq which-key-idle-delay 0.9)
-  :config
-  (which-key-mode))
-
-;; Keyboard-centric user interface
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)          ; Disable the toolbar
-(menu-bar-mode -1)	    ; Disable the menu bar
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(global-hl-line-mode +1)    ; Highlight line
-(delete-selection-mode 1)   ; Delete selection
-;; (tooltip-mode -1)           ; Disable tooltips
-(set-default-coding-systems 'utf-8)
-(setq backup-directory-alist '(("." . "~/.saves")))   ; Different backup directory
-(column-number-mode)
-(add-hook 'prog-mode-hook 'display-line-numbers-mode) ; Display line numbers when programming
+(cond
+ ((string-equal system-type "darwin") ; macOS
+  (set-font "Monaco-14"))
+ ((string-equal system-type "windows-nt") ; Windows
+  (set-font "Consolas-12"))
+ ((string-equal system-type "gnu/linux") ; Linux
+  (set-font "DejaVu Sans Mono-12")))
 
 (use-package projectile
   :ensure t
@@ -112,188 +234,48 @@
   :config
   (dashboard-setup-startup-hook))
 
-(use-package swiper
-  :ensure t)
 
-;; Better Completions with Ivy
-(use-package ivy :ensure t
-  :diminish
-  :bind
-  (("C-s" . swiper)
-   :map ivy-minibuffer-map
-   ("TAB" . ivy-alt-done)
-   ("C-f" . ivy-alt-done)
-   ("C-l" . ivy-alt-done)
-   ("C-j" . ivy-next-line)
-   ("C-k" . ivy-previous-line)
-   :map ivy-switch-buffer-map
-   ("C-k" . ivy-previous-line)
-   ("C-l" . ivy-done)
-   ("C-d" . ivy-switch-buffer-kill)
-   :map ivy-reverse-i-search-map
-   ("C-k" . ivy-previous-line)
-   ("C-d" . ivy-reverse-i-search-kill))
-  :init
-  (ivy-mode 1)
-  :config
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-wrap t)
-  (setq ivy-count-format "(%d/%d) ")
-  (setq enable-recursive-minibuffers t)
-  ;; Use different regex strategies per completion command
-  (push '(completion-at-point . ivy--regex-fuzzy) ivy-re-builders-alist) ;; This doesn't seem to work...
-  (push '(swiper . ivy--regex-ignore-order) ivy-re-builders-alist)
-  (push '(counsel-M-x . ivy--regex-ignore-order) ivy-re-builders-alist)
-
-  ;; Set minibuffer height for different commands
-  (setf (alist-get 'counsel-projectile-ag ivy-height-alist) 15)
-  (setf (alist-get 'counsel-projectile-rg ivy-height-alist) 15)
-  (setf (alist-get 'swiper ivy-height-alist) 15)
-  (setf (alist-get 'counsel-switch-buffer ivy-height-alist) 7))
-
-(use-package counsel
-  :ensure t
-  :demand t
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 ("C-M-j" . counsel-switch-buffer)
-	 ("C-M-l" . counsel-imenu)
-	 :map minibuffer-local-map
-	 ("C-r" . counsel-minibuffer-history))
-  :custom
-  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
-  :config
-  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
-
-(use-package ivy-rich
-  :ensure t
-  :init
-  (ivy-rich-mode 1)
-  :after counsel
-  :config
-  (setq ivy-format-function #'ivy-format-function-line)
-  (setq ivy-rich-display-transformers-list
-        (plist-put ivy-rich-display-transformers-list
-                   'ivy-switch-buffer
-                   '(:columns
-                     ((ivy-rich-candidate (:width 40))
-                      (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right)); return the buffer indicators
-                      (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))          ; return the major mode info
-                      (ivy-rich-switch-buffer-project (:width 15 :face success))             ; return project name using `projectile'
-                      (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))  ; return file path relative to project root or `default-directory' if project is nil
-                     :predicate
-                     (lambda (cand)
-                       (if-let ((buffer (get-buffer cand)))
-                           ;; Don't mess with EXWM buffers
-                           (with-current-buffer buffer
-                             (not (derived-mode-p 'exwm-mode)))))))))
-
-(use-package flx  ;; Improves sorting for fuzzy-matched results
-  :after ivy
-  :defer t
-  :init
-  (setq ivy-flx-limit 10000))
-
-(use-package prescient :ensure t
-  :after counsel
-  :config
-  (prescient-persist-mode 1))
-
-(use-package ivy-prescient :ensure t
-  :after prescient
-  :config
-  (ivy-prescient-mode 1))
-
-;; Snippets
- (use-package yasnippet
-       :ensure t
-       :init
-       (yas-global-mode 1)
-       :config
-       (add-to-list 'yas-snippet-dirs (locate-user-emacs-file "snippets")))
-
-;; Window Selection with ace-window
-(use-package ace-window
-  :bind (("M-o" . ace-window))
-  :custom
-  (aw-scope 'frame)
-  (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-  (aw-minibuffer-flag t)
-  :config
-  (ace-window-display-mode 1))
-
-;; Highlight Matching Braces
-(use-package paren
-  :config
-  (set-face-attribute 'show-paren-match-expression nil :background "#363e4a")
-  (show-paren-mode 1))
-
-(use-package expand-region
-  :ensure t
-  :bind
-  ("C-*" . er/expand-region)
-  ("C--" . er/contract-region))
-
-;; Window tabs
-(use-package centaur-tabs
-  :ensure t
-  :config
-  (setq centaur-tabs-set-bar 'over
-	centaur-tabs-set-icons t
-	centaur-tabs-gray-out-icons 'buffer
-	centaur-tabs-height 24
-	centaur-tabs-set-modified-marker t
-	centaur-tabs-modified-marker "o")
-  (centaur-tabs-mode t))
-
-;; Autocomplate
+;;; Autocomplate
 (use-package company
-  :ensure t
-  :init
-  (add-hook 'after-init-book 'company-tabnine))
-
-;; Trigger completion immediately.
-(setq company-idle-delay 0)
-
-;; Number the candidates (use M-1, M-2 etc to select completions).
-(setq company-show-numbers t)
+  :ensure t)
 
 (use-package flycheck
   :ensure t
   :hook (after-init . global-flycheck-mode))
 
-(use-package php-mode
-  :hook (php-mode . flycheck))
+;;; Tide mode and company mode configuration for TypeScript, JavaScript, and React
+(use-package rjsx-mode
+  :mode ("\\.jsx?\\'" . rjsx-mode)
+  :hook (rjsx-mode . setup-tide-mode))
 
-;; HTML
-(use-package web-mode
-  :mode "(\\.\\(html?\\|ejs\\)\\'"
-  :config
-  (setq-default web-mode-code-indent-offset 2)
-  (setq-default web-mode-markup-indent-offset 2)
-  (setq-default web-mode-attribute-indent-offset 2))
+(use-package typescript-mode
+  :mode ("\\.ts\\'" . typescript-mode)
+  :hook (typescript-mode . setup-tide-mode))
+
+(use-package js2-mode
+  :hook (js2-mode . setup-tide-mode))
+
+(use-package tide
+	:ensure t
+  :hook ((rjsx-mode . setup-tide-mode)
+         (typescript-mode . setup-tide-mode)
+         (js2-mode . setup-tide-mode)))
 
 (defun setup-tide-mode()
   (interactive)
-  (when (or (locate-dominating-file default-directory "tsconfig.json")
-            (locate-dominating-file default-directory "jsconfig.json"))
-    (tide-setup)
-    (flycheck-mode +1)
-    (setq flycheck-check-syntax-automatically '(save mode-enabled))
-    (setq-default javascript-intent-level 2)
-    (tide-hl-identifier-mode +1)
-    (company-mode +1)))
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (tide-hl-identifier-mode +1)
+	(setq js-indent-level 2)
+  (company-mode +1))
 
-;; js
-(use-package rjsx-mode
-  :ensure t
-  :mode "\\.js\\'")
+(add-hook 'tide-mode-hook #'company-mode)
 
-(use-package tide
-  :ensure t
-  :after (rjsx-mode company flycheck)
-  :hook (rjsx-mode . setup-tide-mode))
+
+
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -301,13 +283,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rjsx-mode web-mode projectile tide php-mode flx ace-window ivy-prescient prescient ivy-rich dashboard doom-modeline flycheck company centaur-tabs expand-region all-the-icons doom-themes use-package)))
+   '(tide typescript-mode rjsx-mode flycheck company dashboard projectile doom-modeline all-the-icons gruvbox-theme ace-window centaur-tabs expand-region flx ivy-rich ivy-prescient prescient counsel swiper which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:height 0.85))))
- '(mode-line-inactive ((t (:height 0.85)))))
-
-
+ )
